bac7c66335fe986ef39406f39449eb2418c6fd7a
mkg <mkg@mkg>
2022-04-24 14:10:35 +0100

    CSS Language server path corrected

--- init.lua
+++ init.lua
@@ -558,7 +558,7 @@ if vim.g.setup_lsp == nil then
 	-------------------------------------------------------------------------------------------------------------------------------
 	lsp.html.setup(coq.lsp_ensure_capabilities({
 		cmd = {
-			'C:/Users/Michael/AppData/Local/nvim-data/lsp_servers/html/node_modules/.bin/vscode-css-language-server.cmd',
+			'C:/Users/Michael/AppData/Local/nvim-data/lsp_servers/html/node_modules/.bin/vscode-html-language-server.cmd',
 			'--stdio',
 		},
 	}))
@@ -568,7 +568,7 @@ if vim.g.setup_lsp == nil then
 	-------------------------------------------------------------------------------------------------------------------------------
 	lsp.cssls.setup(coq.lsp_ensure_capabilities({
 		cmd = {
-			'C:/Users/Michael/AppData/Local/nvim-data/lsp_servers/cssls/node_modules/.bin',
+			'C:/Users/Michael/AppData/Local/nvim-data/lsp_servers/cssls/node_modules/.bin/vscode-css-language-server.cmd',
 			'--stdio',
 		},
 	}))
